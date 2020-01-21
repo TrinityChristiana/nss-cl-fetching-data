7666bd69806ce95c7dec0e6c1b47647fa4581e50
Trinity Terry <trinitycterry@gmail.com>
2020-01-21 11:41:54 -0600

    added more functions

--- script.js
+++ script.js
@@ -4,15 +4,36 @@ const url = `http://localhost:8088/food` /*(this url is the food-api from ../c10
 // Happens before fetch 
 console.log("before the fetch");
 
+// Loops through data and prints it to console
+const printData = (allData) => {
+    allData.forEach(data => {
+            console.log(data);
+    });
+};
+
+// Creates html for data
+const dataFactory = data => `<section>${data.name}</section>`;
+
+// renders data HTML to the DOM
+const addDataToDom = dataHTML =>{
+    const container = document.querySelector("#container");
+    container.innerHTML += dataHTML;
+}
+
 // Fetch data from url
 fetch(url)
     // Then do something with it
     .then(response /* taco */ => response.json())/*Take response and convert/parse it to json */
     // get access to json data
-    .then(data /* holds data of json (taco) */=> {
-        // Will console log value of data variable
-        console.log(data);
-        // Happens after fetch because it's runs in order of the fetch's rules
+    .then(jsonData /* holds data of json (taco) */=> {
+        /* // Will console log value of data variable
+        printData(jsonData); */
+        jsonData.forEach(data => {
+            const dataHTML = dataFactory(data);
+            addDataToDom(dataHTML);
+        });
+
+        // Happens inside then, which will run after fetch is done.
         console.log("Really after the fetch???");
     });
 
